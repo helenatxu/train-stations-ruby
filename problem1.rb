f44dbae13175525185b936c9bc10f7871e5665a2
Helena Moreno Chacon <helenatxu@gmail.com>
2012-12-17 22:53:14 -0800

    Working in 6

--- problem1.rb
+++ problem1.rb
@@ -17,36 +17,20 @@ class TrainStations
     if x.nil? || y.nil? 
       return nil
     end
-
-    #puts x 
-    #puts " <--- X"
-    #puts y
-    #puts " <--- Y"
-
     if !@routes.has_key?(x)
       return nil
     elsif @routes[x][y].nil?
       return nil
     else
-    #  puts @routes[x][y]
-    #  puts " <--- @routes[x][y] \n"
       return @routes[x][y]
     end
   end
 
-
-  def calc_dist (a)
+  def calcDist (a)
     i = 0 
     total = 0 
     dist = 0
-    #TODO mejorar con yield
-    #puts a
-    #puts a.length
-    #puts "--------"
     while i < a.length-1
-      #puts i   
-      #puts " <--- i"
-
       dist = distance a[i], a[i+1]
       i+=1
       if dist.nil?
@@ -55,13 +39,35 @@ class TrainStations
         total += dist
       end      
     end
-    #puts "Total: " + total.to_s
     return total
     puts "\n"
   end
 
+  def countTrips (origin, destination, maxStops)
+    trips = 0
+    puts @routes[origin]
+    @routes[origin].each do |node, value|
+      puts node
+      trips = isThereTrip(node, destination, maxStops, 0)
+    end
+    puts "Final: "
+    puts trips
+    return trips
+  end
 
+  def isThereTrip (node, destination, count, trips)
+    puts trips
+    if count === 0
+      return trips
+    elsif node == destination 
+      trips += 1
+    end
+    @routes[node].each do |n, v|
+      isThereTrip(n, destination, count-1, trips)
+    end
+  end
 end
+
 
 stations = TrainStations.new
 
@@ -69,16 +75,10 @@ stations = TrainStations.new
 ###   TESTS  ###
 puts "\n"
 
-# puts stations.calc_dist ["A","B","C"]
-# puts stations.calc_dist ["A","C"]
-# puts stations.calc_dist ["A","Z"]
-# puts stations.calc_dist [nil,"B","C"]
-# puts stations.calc_dist ["1","B","C"]
-# puts stations.calc_dist ["A","B","C","D","E"]
-# puts stations.calc_dist ["A","B","C","D","C"]
+# stations.calcDist ["A","B","C"]           #1. The distance of the route A­B­C.
+# stations.calcDist ["A","D"]               #2. The distance of the route A­D.
+# stations.calcDist ["A","D","C"]           #3. The distance of the route A­D­C.
+# stations.calcDist ["A","E", "B","C", "D"] #4. The distance of the route A­E­B­C­D. 
+# stations.calcDist ["A","E","D"]           #5. The distance of the route A­E­D.
 
-puts stations.calc_dist ["A","B","C"]           #1. The distance of the route A­B­C.
-puts stations.calc_dist ["A","D"]               #2. The distance of the route A­D.
-puts stations.calc_dist ["A","D","C"]           #3. The distance of the route A­D­C.
-puts stations.calc_dist ["A","E", "B","C", "D"] #4. The distance of the route A­E­B­C­D. 
-puts stations.calc_dist ["A","E","D"]           #5. The distance of the route A­E­D.
+stations.countTrips "C", "C", 3
